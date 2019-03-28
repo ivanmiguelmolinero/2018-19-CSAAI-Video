7d45622c30cc4cecedd7978ddb877578c53d1f9e
Ivan Miguel Molinero <ivan@beta05.aulas.gsyc.urjc.es>
2019-03-28 11:51:30 +0100

    Inicio

---
+++ video.js
@@ -0,0 +1,12 @@
+function main ()
+{
+  video1 = document.getElementById("video1");
+
+  video2 = document.getElementById("video2");
+
+  video3 = document.getElementById("video3");
+
+  video2.height = 286;
+
+  video3.height = 286;
+}
