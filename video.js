b005496b9df94ca5751bdd7997b21f4b2295a457
ivanmiguelmolinero <i.miguel.2016@alumnos.urjc.es>
2019-04-01 20:00:32 +0200

    Parche 0.4

--- video.js
+++ video.js
@@ -6,7 +6,80 @@ function main ()
 
   video3 = document.getElementById("video3");
 
-  video2.height = 286;
+  videocentral = document.getElementById("videocentral");
 
-  video3.height = 286;
+  video1ON = false;
+  video2ON = false;
+  video3ON = false;
+
+  reloj = document.getElementById("reloj");
+
+  timer = null;
+
+  video1.onmouseover = () => {
+    video1.muted = false;
+  }
+  video1.onmouseout = () => {
+    video1.muted = true;
+  }
+
+  video1.onclick = () => {
+    video1.muted = true;
+    videocentral.src = "swamenazafantasma.mp4";
+    videocentral.currentTime = video1.currentTime;
+    video1ON = true;
+    video2ON = false;
+    video3ON = false;
+  }
+
+  video2.onmouseover = () => {
+    video2.muted = false;
+  }
+  video2.onmouseout = () => {
+    video2.muted = true;
+  }
+
+  video2.onclick = () => {
+    video2.muted = true;
+    videocentral.src = "mivideo.mp4";
+    videocentral.currentTime = video2.currentTime;
+    video1ON = false;
+    video2ON = true;
+    video3ON = false;
+  }
+
+  video3.onmouseover = () => {
+    video3.muted = false;
+  }
+  video3.onmouseout = () => {
+    video3.muted = true;
+  }
+
+  video3.onclick = () => {
+    video3.muted = true;
+    videocentral.src = "sw.mp4";
+    videocentral.currentTime = video3.currentTime;
+    video1ON = false;
+    video2ON = false;
+    video3ON = true;
+  }
+
+  if (!timer) {
+    timer = setInterval( () => {
+      if (video1ON) {
+        var str = new String(video1.currentTime);
+        var pos = str.indexOf(".");
+        reloj.innerHTML = str.slice(0,pos);
+      } else if (video2ON) {
+        var str = new String(video2.currentTime);
+        var pos = str.indexOf(".");
+        reloj.innerHTML = str.slice(0,pos);
+      } else if (video3ON) {
+        var str = new String(video3.currentTime);
+        var pos = str.indexOf(".");
+        reloj.innerHTML = str.slice(0,pos);
+      }
+    }, 100)
+  }
+
 }
